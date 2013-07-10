71947e914ebc24f7e63bcf56b7f7ab1c844839cd
Ubuntu <ubuntu@ip-172-31-38-90.us-west-2.compute.internal>
2013-07-10 02:33:04 +0000

    HW3 update

--- grader.js
+++ grader.js
@@ -1,13 +1,11 @@
 #! /usr/bin/env node
 
-var fs = require('fs')
+var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
 var restler = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://mysterious-reef-5629.herokuapp.com";
-
 
 var assertFileExists = function(infile){
     var instr = infile.toString();
@@ -41,16 +39,24 @@ var clone = function(fn){
     return fn.bind({});
 };
 
+var process = function(){
+};
+
 if(require.main == module){
     program
     .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
     .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-    .option('-u, --url <site_url>', 'Site URL', clone(assertFileExists), URL_DEFAULT)
+    .option('-u, --url <site_url>', 'Site URL')
     .parse(process.argv);
 
     var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url){
+	console.log('URL');
+	restler.get(program.url).on('complete', process);
+    } else {    	
+	var outJson = JSON.stringify(checkJson, null, 4);
+	console.log(outJson);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
